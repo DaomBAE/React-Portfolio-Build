6774c8d203e8021ea9fc0e7bf03d2d9f39a620ba
DaomBae <da6970@gmail.com>
2023-03-30 11:14:00 +0900

    classname

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import"./App.css"
+
 function App() {
   return (
   <div>
-    <h1>Hello React!</h1>
+    <h1 className="hello">Hello, React!</h1>
     <h2>This is Daom Bae</h2>
     <h3>My favorite coffee is De Costa Rica ☕ </h3>
     <h3>I want to be a millionaire</h3>
