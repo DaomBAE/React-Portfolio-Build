ac75adf655ddcf94cd7ffe78266e9b8811fcc3d2
DaomBae <da6970@gmail.com>
2023-03-30 15:57:50 +0900

    Favorite

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import"./App.css"
-
 function App() {
   return (
-  <div>
-    <h1 className="hello">Hello, React!</h1>
-    <h2>This is Daom Bae</h2>
-    <h3>My favorite coffee is De Costa Rica ☕ </h3>
-    <h3>I want to be a millionaire</h3>
-    <h1>Thank you for watching</h1>
-  </div>
+    <div className="bg-red-100 min-h-screen">
+      <header className="bg-blue-100 h-12 md:h-20">
+        <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+          <div>Daom Bae Portfolio</div>
+          <ul className="flex gap-2 md:gap-8 text-xs sm:text-sm md:text-base">
+            <li className="bg-purple-300 btn-style">Introduce</li>
+            <li className="bg-cyan-300 btn-style">Portfolio</li>
+            <li className="bg-indigo-300 btn-style">Contact Me</li>
+          </ul>
+        </div>
+
+      <main>
+        <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          Hey,
+        </div>
+      </main>
+      </header>
+    </div>
   );
 }
+
 
 export default App;
